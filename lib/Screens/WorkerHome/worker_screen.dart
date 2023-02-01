8fa19ad2b44f6f986c290e2a4a1a08fb4d6cc6ca
jasperlawrencee <caballerojasper.55555@gmail.com>
2023-02-02 00:42:02 +0800

    ez home screen

--- lib/Screens/WorkerHome/worker_screen.dart
+++ lib/Screens/WorkerHome/worker_screen.dart
@@ -43,7 +43,7 @@ class _MobileWorkerScreenState extends State<MobileWorkerScreen> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            "Welcome",
+            "Welcome Louise!",
             style: TextStyle(
                 fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
           ),
@@ -100,7 +100,7 @@ class _MobileWorkerScreenState extends State<MobileWorkerScreen> {
           physics: BouncingScrollPhysics(),
           children: [
             itemChat(
-                avatar: 'assets/avatars/2.jpg',
+                avatar: 'assets/avatars/3.jpg',
                 name: 'Jane Doe',
                 chat:
                     'Lorem impsum is simply the dummy text of the printing and typesetting industry',
@@ -112,50 +112,57 @@ class _MobileWorkerScreenState extends State<MobileWorkerScreen> {
   }
 
   Widget itemChat({String avatar = '', name = '', chat = '', time = '00.00'}) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        vertical: 20,
-      ),
-      child: Row(children: [
-        Avatar(
-          margin: EdgeInsets.only(right: 20),
-          size: 60,
-          image: avatar,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: ((context) => ChatPage())));
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          vertical: 20,
         ),
-        Expanded(
-            child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '$name',
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.bold,
+        child: Row(children: [
+          Avatar(
+            margin: EdgeInsets.only(right: 20),
+            size: 60,
+            image: avatar,
+          ),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '$name',
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Text(
-                  '$time',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '$chat',
-              overflow: TextOverflow.ellipsis,
-              style:
-                  TextStyle(color: Colors.black54, fontWeight: FontWeight.bold),
-            )
-          ],
-        ))
-      ]),
+                  Text(
+                    '$time',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '$chat',
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    color: Colors.black54, fontWeight: FontWeight.bold),
+              )
+            ],
+          ))
+        ]),
+      ),
     );
   }
 }
@@ -183,6 +190,221 @@ class Avatar extends StatelessWidget {
             image: DecorationImage(
               image: AssetImage(image),
             )),
+      ),
+    );
+  }
+}
+
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: SafeArea(
+        child: Stack(children: [
+          Column(
+            children: [
+              topChat(),
+              bottomChat(),
+            ],
+          ),
+          formChat(),
+        ]),
+      ),
+    );
+  }
+
+  Widget topChat() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 25),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: Icon(
+                  Icons.arrow_back_ios,
+                  size: 25,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Jane Doe',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.black12,
+                ),
+                child: Icon(
+                  Icons.call,
+                  size: 25,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Container(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.black12,
+                ),
+                child: Icon(
+                  Icons.video_call,
+                  size: 25,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget bottomChat() {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.only(left: 25, right: 25, top: 25),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(45),
+            topRight: Radius.circular(45),
+          ),
+          color: Colors.white,
+        ),
+        child: ListView(
+          physics: BouncingScrollPhysics(),
+          children: [
+            itemChat(
+                avatar: 'assets/avatars/5.jpg',
+                chat: 1,
+                message:
+                    'Lorem impsum is simply the dummy text of the printing and typesetting industry',
+                time: '18:00'),
+            itemChat(
+                avatar: 'assets/avatars/5.jpg',
+                chat: 0,
+                message:
+                    'Lorem impsum is simply the dummy text of the printing and typesetting industry',
+                time: '18:00'),
+            itemChat(
+                avatar: 'assets/avatars/5.jpg',
+                chat: 1,
+                message: "Okay po, I'll book the transaction",
+                time: '19:00'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // if chat = 0 : Send
+  // if chat = 1 : Recieved
+  Widget itemChat({required int chat, required String avatar, message, time}) {
+    return Row(
+      mainAxisAlignment:
+          chat == 0 ? MainAxisAlignment.end : MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        chat == 1
+            ? Avatar(
+                image: avatar,
+                size: 50,
+              )
+            : Text(
+                '$time',
+                style: TextStyle(color: Colors.grey.shade400),
+              ),
+        Flexible(
+          child: Container(
+            margin: EdgeInsets.only(left: 10, right: 10, top: 20),
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: chat == 0 ? kPrimaryLightColor : Colors.indigo.shade50,
+                borderRadius: chat == 0
+                    ? BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                        bottomLeft: Radius.circular(30),
+                      )
+                    : BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                        bottomRight: Radius.circular(30),
+                      )),
+            child: Text('$message'),
+          ),
+        ),
+        chat == 1
+            ? Text(
+                '$time',
+                style: TextStyle(color: Colors.grey.shade400),
+              )
+            : SizedBox()
+      ],
+    );
+  }
+
+  Widget formChat() {
+    return Positioned(
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: 120,
+          padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: kPrimaryLightColor,
+              labelStyle: TextStyle(
+                fontSize: 12,
+              ),
+              contentPadding: EdgeInsets.all(20),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: kPrimaryLightColor),
+                  borderRadius: BorderRadius.circular(25)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: kPrimaryLightColor),
+                  borderRadius: BorderRadius.circular(25)),
+              prefixIcon: Container(
+                child: Icon(Icons.add),
+              ),
+              suffixIcon: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: kPrimaryColor,
+                ),
+                child: Icon(
+                  Icons.send_rounded,
+                  color: Colors.white,
+                  size: 28,
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }

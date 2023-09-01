d1f65b50a2d8926db6afbc8df3a0586ab3027ff0
jasperlawrencee <caballerojasper.55555@gmail.com>
2023-09-02 07:57:52 +0800

    firebase_auth added

--- lib/Screens/Login/login_screen.dart
+++ lib/Screens/Login/login_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: must_be_immutable
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/Signup/signup_screen.dart';
 import 'package:flutter_auth/components/already_have_an_account_acheck.dart';
@@ -22,12 +23,57 @@ class LoginScreen extends StatelessWidget {
   }
 }
 
-class MobileLoginScreen extends StatelessWidget {
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  final TextEditingController username = TextEditingController();
+class MobileLoginScreen extends StatefulWidget {
+  const MobileLoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MobileLoginScreen> createState() => _MobileLoginScreenState();
+}
+
+class _MobileLoginScreenState extends State<MobileLoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  bool isLoading = false;
+  final TextEditingController email = TextEditingController();
   final TextEditingController password = TextEditingController();
 
-  MobileLoginScreen({Key? key}) : super(key: key);
+  //firebase auth call function
+  signInWithEmailAndPassword() async {
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email.text,
+        password: password.text,
+      );
+      setState(() {
+        isLoading = false;
+      });
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      if (e.code == 'user-not-found') {
+        return ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Invalid or user not found"),
+            behavior: SnackBarBehavior.floating,
+          ),
+        );
+      } else if (e.code == 'wrong-password') {
+        return ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Wrong password"),
+            behavior: SnackBarBehavior.floating,
+          ),
+        );
+      }
+    }
+  }
+
+  final snackbar = const SnackBar(
+    content: Text("Logged In"),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -40,59 +86,84 @@ class MobileLoginScreen extends StatelessWidget {
             const Spacer(),
             Expanded(
                 flex: 8,
-                child: Column(
-                  children: <Widget>[
-                    textField(
-                      "Your Username",
-                      Icons.person,
-                      false,
-                      username,
-                    ),
-                    const SizedBox(
-                      height: defaultPadding,
-                    ),
-                    textField(
-                      "Your Password",
-                      Icons.lock,
-                      true,
-                      password,
-                    ),
-                    const SizedBox(
-                      height: defaultPadding,
-                    ),
-                    SignInSignUp(context, true, () {
-                      username.text.isNotEmpty && password.text.isNotEmpty
-                          ? loginUser()
-                          : const AlertDialog(
-                              content:
-                                  Text("Please write username and password"),
-                            );
-                    }),
-                    AlreadyHaveAnAccountCheck(
-                      login: true,
-                      press: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return const SignUpScreen();
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: <Widget>[
+                      textField(
+                        "Email Address",
+                        Icons.person,
+                        false,
+                        email,
+                      ),
+                      const SizedBox(
+                        height: defaultPadding,
+                      ),
+                      textField(
+                        "Password",
+                        Icons.lock,
+                        false,
+                        password,
+                      ),
+                      const SizedBox(
+                        height: defaultPadding,
+                      ),
+                      SizedBox(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 50,
+                          margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(90)),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (formKey.currentState!.validate()) {
+                                print("email and password clear");
+                                // signInWithEmailAndPassword();
+                              }
                             },
+                            child: isLoading
+                                ? const Center(
+                                    child: CircularProgressIndicator(
+                                    color: Colors.white,
+                                  ))
+                                : const Text(
+                                    'LOG IN',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 13,
+                                      fontFamily: 'Inter',
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
                           ),
-                        );
-                      },
-                    ),
-                  ],
+                        ),
+                      ),
+                      // SignInSignUp(context, true, () {
+                      //   if (formKey.currentState!.validate()) {
+                      //     signInWithEmailAndPassword();
+                      //   }
+                      // }),
+                      AlreadyHaveAnAccountCheck(
+                        login: true,
+                        press: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return const SignUpScreen();
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 )),
             const Spacer(),
           ],
         ),
       ],
-    );
-  }
-
-  void loginUser() async {
-    const AlertDialog(
-      content: Text("Authenticating, Please Wait..."),
     );
   }
 }
---
+++ lib/features/user_auth/firebase_auth.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signUpWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      UserCredential credential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user;
+    } catch (e) {
+      print("firebase auth Error has occured");
+    }
+    return null;
+  }
+
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+}

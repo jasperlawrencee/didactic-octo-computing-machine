9dd64fd223a429aa67865f92f970c81e424eb64b
jasperlawrencee <caballerojasper.55555@gmail.com>
2023-09-08 09:31:56 +0800

    try

--- lib/Screens/HomeScreens/Worker/profile_screen.dart
+++ lib/Screens/HomeScreens/Worker/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/components/background.dart';
+import 'package:flutter_auth/constants.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -11,12 +12,47 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Background(
-          child: Container(
-        margin: const EdgeInsets.fromLTRB(15, 35, 15, 0),
-        child: Column(
-          children: [Text("Profile Page")],
+          child: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.fromLTRB(15, 35, 15, 0),
+          child: Column(
+            children: [
+              Text(
+                "Notifications".toUpperCase(),
+                style: const TextStyle(
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: kPrimaryColor,
+                ),
+              ),
+              Container(
+                height: height * 0.4,
+                color: kPrimaryColor,
+                child: LayoutBuilder(builder: (contex, constraints) {
+                  double innerHeight = constraints.maxHeight;
+                  double innerWidth = constraints.maxWidth;
+                  return Stack(
+                    children: [
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          height: innerHeight * 0.65,
+                          color: kPrimaryLightColor,
+                        ),
+                      )
+                    ],
+                  );
+                }),
+              ),
+            ],
+          ),
         ),
       )),
     );

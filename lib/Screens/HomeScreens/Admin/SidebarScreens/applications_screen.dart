0189ab2d58634a4c0fd1d5304c70e5e63e19ac43
jasperlawrencee <caballerojasper.55555@gmail.com>
2023-10-11 01:39:04 +0800

    yes

--- lib/Screens/HomeScreens/Admin/SidebarScreens/applications_screen.dart
+++ lib/Screens/HomeScreens/Admin/SidebarScreens/applications_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/constants.dart';
 
@@ -9,8 +13,13 @@ class Applications extends StatefulWidget {
 }
 
 class _ApplicationsState extends State<Applications> {
+  User? currentUser = FirebaseAuth.instance.currentUser;
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
   List<String> type = ['All', 'Salon', 'Freelancer'];
   List<String> verification = ['Unverified', 'Verified'];
+  String firstName = '';
+  String middleName = '';
+  String lastName = '';
 
   @override
   Widget build(BuildContext context) {
@@ -44,10 +53,41 @@ class _ApplicationsState extends State<Applications> {
                           value: value, label: value);
                     }).toList()),
               ],
+            ),
+            InkWell(
+              child: Container(
+                height: 150,
+                width: 220,
+                color: kPrimaryLightColor,
+              ),
+              onTap: () {},
             )
           ],
         ),
       ),
     );
+  }
+
+  //must grab all users in firestore
+  getUserFullName() {
+    _firebaseFirestore
+        .collection('users')
+        .doc(currentUser!.uid)
+        .collection('userDetails')
+        .doc('step1')
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      try {
+        if (documentSnapshot.exists) {
+          setState(() {
+            firstName = documentSnapshot.get('firstName');
+            middleName = documentSnapshot.get('middleName');
+            lastName = documentSnapshot.get('lastname');
+          });
+        }
+      } catch (e) {
+        log(e.toString());
+      }
+    });
   }
 }
